b063537ca5ec514beb2594fc2ccd71d1a80b671d
Joao Pereira <nashi.codes@gmail.com>
2022-07-23 19:48:43 -0300

    criacao do submit listener

--- form.js
+++ form.js
@@ -1,6 +1,14 @@
+function criaCadastro(Nome, idade, nivel) {
+  
+}
 document.cadastro.addEventListener("submit", submitListener);
+
 function submitListener(event) {
   event.preventDefault();
-
+  criaCadastro(
+    document.cadastro.Nome.value,
+    Number(document.cadastro.idade.value),
+    document.cadastro.nivel.value
+  )
   document.cadastro.reset();
 }
