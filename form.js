9d1b388adecbb35ba069740bd9a945717e6abcc4
Joao Pereira <nashi.codes@gmail.com>
2022-07-23 21:55:18 -0300

    alterado a apresentacao das listas

--- form.js
+++ form.js
@@ -1,5 +1,12 @@
-function criaCadastro(Nome, idade, nivel) {
-  
+function criaCadastro(Nome, idade) {
+  if(idade<18){
+    const jr = document.createElement("li");
+    jr.textContent = `${Nome}, ${idade}`;
+    document.ulJunior.appendChild(jr);
+  }
+  if(idade>40){
+
+  }
 }
 document.cadastro.addEventListener("submit", submitListener);
 
@@ -7,8 +14,7 @@ function submitListener(event) {
   event.preventDefault();
   criaCadastro(
     document.cadastro.Nome.value,
-    Number(document.cadastro.idade.value),
-    document.cadastro.nivel.value
+    Number(document.cadastro.idade.value)
   )
   document.cadastro.reset();
 }
